40d603b1dd82d12adbeef7374749cfb8a46bd63f
bookgom1015 <bookgom1015@naver.com>
2022-04-02 21:22:39 +0900

    Fixed regular expressions(to support Safari)

--- src/components/DecimalSeperator.js
+++ src/components/DecimalSeperator.js
@@ -1,3 +1,6 @@
+// /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g -- This regular expressions isn't supported in Mac;;
+// /\B(?=(\d{3})+(?!\d))/g -- Use this instead
+
 export default function setComma(expr) {
-    return expr.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    return expr.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
